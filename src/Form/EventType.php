331c6763933afd7fd07a3ad54b6e0a7411313ab5
KanThor73 <you@example.com>
2023-09-11 16:57:44 +0200

    creation eventController - creation eventForm

---
+++ src/Form/EventType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('beginDate')
+            ->add('duration')
+            ->add('limitDate')
+            ->add('nbMaxInscription')
+            ->add('infoEvent')
+            ->add('place')
+            ->add('state')
+            ->add('users')
+            ->add('campus')
+            ->add('organizer')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
