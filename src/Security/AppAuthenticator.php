91c41be3cf9a2a51655c0547266abe19c62df15e
Sam <sami.grolleau@mailo.com>
2023-09-12 11:14:21 +0200

    correction de config, mais toujours pas de déconnexion

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -45,12 +45,11 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectToRoute('home');
     }
 
     protected function getLoginUrl(Request $request): string
