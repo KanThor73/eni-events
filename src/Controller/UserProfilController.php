713e91b649e62e05677bb32d02f8c8125f8e3c77
Sam <sami.grolleau@mailo.com>
2023-09-14 15:14:16 +0200

    userprofil : mot de passé caché, hashé, et facultatif

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Form\Exception\InvalidArgumentException;
 
 
@@ -17,6 +18,7 @@ class UserProfilController extends AbstractController
     #[Route('/user/profil', name: 'app_user_profil')]
     public function create(
         Request $request,
+	UserPasswordHasherInterface $userPasswordHasher,
         EntityManagerInterface $entityManager
     ): Response
     {
@@ -26,15 +28,21 @@ class UserProfilController extends AbstractController
         $userForm->handleRequest($request);
 
         if ($userForm->isSubmitted()&&$userForm->isValid()){
-            dd($userForm);
-            $entityManager->persist($user);
+
+            if ($userForm->get('plainPassword')->getData() != null) { // si la personne saisit un nouveau mot de passe
+	    	$user->setPassword(
+	    		$userPasswordHasher->hashPassword(
+				$user,
+				$userForm->get('plainPassword')->getData()
+			)
+	    	);
+	    }
+
+	    $entityManager->persist($user);
             $entityManager->flush();
 
             $this->addFlash('success', 'Nouveau participant!');
            //return $this->redirectToRoute('app_show_profil' ['id' => $user->getId()])
-
-
-
         }
 
         return $this->render('user_profil/userProfil.html.twig', [
--- src/Form/UserProfilType.php
+++ src/Form/UserProfilType.php
@@ -6,9 +6,11 @@ use App\Entity\User;
 use App\Entity\Campus;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserProfilType extends AbstractType
 {
@@ -19,8 +21,21 @@ class UserProfilType extends AbstractType
                 'label' => 'Email :',
                 'mapped' => false,
             ])
-            ->add('password', null, [
-                'label' => 'Mot de passe :'
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Nouveau mot de passe :',
+		'mapped' => false,
+		'required' => false,
+                'constraints' => [
+                    #new NotBlank([
+                    #    'message' => 'Please enter a password',
+                    #]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
             ->add('name', null, [
                 'label' => 'Nom :'
