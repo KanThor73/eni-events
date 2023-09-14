2705aae4c75f40cc93169e0685d7e5b87284e7d2
ArtNov90 <art.novara@gmail.com>
2023-09-14 11:52:09 +0200

    tentative accès à la base de données

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -16,20 +16,22 @@ class UserProfilController extends AbstractController
 {
     #[Route('/user/profil', name: 'app_user_profil')]
     public function create(
-        Request                $request,
+        Request $request,
         EntityManagerInterface $entityManager
     ): Response
     {
         $user = $this->getUser();
-        $userForm =  $this->createForm(UserProfilType::class, $user)->handleRequest($request);
+        $userForm =  $this->createForm(UserProfilType::class, $user);
+
+        $userForm->handleRequest($request);
 
         if ($userForm->isSubmitted()&&$userForm->isValid()){
             dd($userForm);
-           // $entityManager->persist($user);
+            $entityManager->persist($user);
             $entityManager->flush();
 
             $this->addFlash('success', 'Nouveau participant!');
-           // return $this->redirectToRoute('app_show_profil' ['id' => $user->getId()])
+           //return $this->redirectToRoute('app_show_profil' ['id' => $user->getId()])
 
 
 
