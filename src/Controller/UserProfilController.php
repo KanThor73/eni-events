5dbfa6ecb07f3ea98460343b36f46b3ed6ff6ff1
ArtNov90 <art.novara@gmail.com>
2023-09-15 11:07:19 +0200

    ajout d'un Route dans UserProfilController pour ajouter une vue du profil

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -15,7 +15,9 @@ use Symfony\Component\Form\Exception\InvalidArgumentException;
 
 class UserProfilController extends AbstractController
 {
-    #[Route('/user/profil', name: 'app_user_profil')]
+
+
+    #[Route('/user', name: 'app_user')]
     public function create(
         Request $request,
 	UserPasswordHasherInterface $userPasswordHasher,
@@ -48,6 +50,22 @@ class UserProfilController extends AbstractController
 
         return $this->render('user_profil/userProfil.html.twig', [
             'userForm' => $userForm->createView()
+        ]);
+    }
+    #[Route('/profil', name: 'app_user_profil')]
+    public function showUserProfile(int $id): Response
+    {
+        // Récupérez l'utilisateur à partir de la base de données en fonction de l'ID
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('L\'utilisateur n\'a pas été trouvé.');
+        }
+
+        // Ensuite, passez l'utilisateur à la vue pour l'afficher ( vue d'exemple la vue twig est à crée)
+        return $this->render('user/show_profile.html.twig', [
+            'user' => $user,
         ]);
     }
 }
