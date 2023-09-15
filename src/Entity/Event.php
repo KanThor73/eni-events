0334e8239beb2c61a96f3377dfe1c2144fe5f5c6
KanThor73 <you@example.com>
2023-09-15 16:18:26 +0200

    resolution pb duration

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -100,12 +100,6 @@ class Event
         return $heures . ':'.$minutes.':'.$secondes;
     }
 
-//    public function setDuration(\DateInterval $duration): static
-//    {
-//        $this->duration = $duration;
-//
-//        return $this;
-//    }
 
     /**
      * @throws Exception
