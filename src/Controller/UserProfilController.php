6fb98df714079e7a8f578607a47c74a945b898e0
KanThor73 <you@example.com>
2023-09-15 15:57:31 +0200

    lien user/showProfil

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -18,37 +18,45 @@ class UserProfilController extends AbstractController
 
     #[Route('/user/profil', name: 'app_user_profil')]
     public function create(
-        Request $request,
-	UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
+        Request                     $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface      $entityManager
     ): Response
     {
         $user = $this->getUser();
 
-        $userForm =  $this->createForm(UserProfilType::class, $user);
+        $userForm = $this->createForm(UserProfilType::class, $user);
 
         $userForm->handleRequest($request);
 
-        if ($userForm->isSubmitted()&&$userForm->isValid()){
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
 
             if ($userForm->get('plainPassword')->getData() != null) { // si la personne saisit un nouveau mot de passe
-	    	$user->setPassword(
-	    		$userPasswordHasher->hashPassword(
-				$user,
-				$userForm->get('plainPassword')->getData()
-			)
-	    	);
-	    }
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $userForm->get('plainPassword')->getData()
+                    )
+                );
+            }
 
-	    $entityManager->persist($user);
+            $entityManager->persist($user);
             $entityManager->flush();
 
             $this->addFlash('success', 'Nouveau participant!');
-           //return $this->redirectToRoute('app_show_profil' ['id' => $user->getId()])
+            //return $this->redirectToRoute('app_show_profil' ['id' => $user->getId()])
         }
 
         return $this->render('user_profil/userProfil.html.twig', [
             'userForm' => $userForm->createView()
+        ]);
+    }
+
+    #[Route('/user/showProfil/{id}', name: 'showProfil')]
+    function showProfil(User $user): Response
+    {
+            return $this->render('user_profil/showProfil.html.twig', [
+            'user' => $user
         ]);
     }
 }
