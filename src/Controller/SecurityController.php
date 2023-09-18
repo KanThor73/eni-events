b7918157a4cfa6030f7d73983d063133e219e612
KanThor73 <you@example.com>
2023-09-18 16:23:29 +0200

    finition css login flash

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\ResetPassword;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SecurityController extends AbstractController
 {
@@ -28,5 +35,42 @@ class SecurityController extends AbstractController
     public function logout(): Response
     {
         // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route(path: '/reset_password', name: 'reset_password')]
+    public function resetPassword(EntityManagerInterface $entityManager,Request $request, UserRepository $userRepository): Response
+    {
+        $emailForm = $this->createFormBuilder()->add('email', EmailType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'le champ email ne peut pas etre vide'
+                ])
+            ]
+        ])->getForm();
+
+        $emailForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+
+            $email = $emailForm->get('email')->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            if ($user) {
+                $resetPassword = new ResetPassword();
+                $resetPassword->setUser($user);
+                $resetPassword->setExpiredAt(new \DateTimeImmutable('+1 hours')); // le token est valide pour 1 heures
+                $token = substr(str_replace(['+', '/', '='], '', base64_decode(random_bytes(30))), 0, 20);
+                $resetPassword->setToken($token);
+                dump($resetPassword);
+            }
+            $this->addFlash('success', 'Un email de reinitialisation viens de vous etre envoye');
+        }
+
+        return $this->render('security/reset_password.html.twig', [
+            'form' => $emailForm->createView()
+        ]);
     }
 }
