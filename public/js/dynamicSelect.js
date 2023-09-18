bbb106a7f9bd9885c0db1c94498fb561c963f4a9
KanThor73 <you@example.com>
2023-09-18 09:04:24 +0200

    clean form eventCreate ajour postCode en standby

--- public/js/dynamicSelect.js
+++ public/js/dynamicSelect.js
@@ -24,30 +24,30 @@ window.onload = () => {
                 }
             )
     });
-
-    let placeName = document.querySelector("#data_location_name");
-    placeName.addEventListener("change", function () {
-        let form = this.closest("form");
-        let data = this.name + "=" + this.value;
-        fetch(form.action, {
-            method: form.getAttribute("method"),
-            body: data,
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
-            }
-        })
-            .then(response => response.text())
-            .then(html => {
-                    console.log(html);
-                    let content = document.createElement("html");
-                    content.innerHTML = html;
-                    let newStreet = content.querySelector("#data_location_street");
-                    let newlatitude = content.querySelector("#data_location_latitude");
-                    let newLongitude = content.querySelector("#data_location_longitude");
-                    document.querySelector("#data_location_street").replaceWith(newStreet);
-                    document.querySelector("#data_location_latitude").replaceWith(newlatitude);
-                    document.querySelector("#data_location_longitude").replaceWith(newLongitude);
-                }
-            )
-    });
+    //
+    // let placeName = document.querySelector("#data_location_name");
+    // placeName.addEventListener("change", function () {
+    //     let form = this.closest("form");
+    //     let data = this.name + "=" + this.value;
+    //     fetch(form.action, {
+    //         method: form.getAttribute("method"),
+    //         body: data,
+    //         headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+    //         }
+    //     })
+    //         .then(response => response.text())
+    //         .then(html => {
+    //                 console.log(html);
+    //                 let content = document.createElement("html");
+    //                 content.innerHTML = html;
+    //                 let newStreet = content.querySelector("#data_location_street");
+    //                 let newlatitude = content.querySelector("#data_location_latitude");
+    //                 let newLongitude = content.querySelector("#data_location_longitude");
+    //                 document.querySelector("#data_location_street").replaceWith(newStreet);
+    //                 document.querySelector("#data_location_latitude").replaceWith(newlatitude);
+    //                 document.querySelector("#data_location_longitude").replaceWith(newLongitude);
+    //             }
+    //         )
+    // });
 }
