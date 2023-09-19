81683992afc3ed0c2008ed21b36d4c8e697ecdb0
KanThor73 <you@example.com>
2023-09-19 13:29:26 +0200

    userPicture nullable

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -54,7 +54,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255,unique: true)]
     private ?string $pseudo = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $picture = null;
 
     public function __construct()
