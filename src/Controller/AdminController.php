28f80bd0b05b3456fb6f000ebf81aa2afc698931
KanThor73 <you@example.com>
2023-09-21 09:10:35 +0200

    delete/ disable user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,4 +18,50 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    #[Route('/admin/manage', name: 'manage')]
+    public function manage(UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/manage.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/admin/delete/{id}', name: 'delete')]
+    public function delete(User $user, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $users = $userRepository->findAll();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->render('admin/manage.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/admin/disable/{id}', name: 'disable')]
+    public function disable(User $user, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user->setRoles(["ROLE_DISABLE"]);
+        $entityManager->flush();
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/manage.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/admin/enable/{id}', name: 'enable')]
+    public function enable(User $user, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $users = $userRepository->findAll();
+        $user->setRoles(["ROLE_USER"]);
+        $entityManager->flush();
+        return $this->render('admin/manage.html.twig', [
+            'users' => $users
+        ]);
+    }
+
 }
